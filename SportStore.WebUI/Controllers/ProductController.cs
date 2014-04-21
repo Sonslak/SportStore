67ead99c156ed316b229b3dca1eb385c43d98881
Sonslak <sonialouw.london@gmail.com>
2014-04-21 18:46:50 +0100

    new

--- SportStore.WebUI/Controllers/ProductController.cs
+++ SportStore.WebUI/Controllers/ProductController.cs
@@ -6,24 +6,40 @@ using System.Web;
 using System.Web.Mvc;
 using SportStore.Domain.Abstract;
 using SportStore.Domain.Entities;
+using SportStore.WebUI.Models;
 
 namespace SportStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
-        //
-        // GET: /Product/
-
         private IProductRepository repository;
+        public int PageSize = 4;
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
-
-
-        public ViewResult List()
+        public ViewResult List(string category, int page = 1)
         {
-            return View(repository.Products);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .Where(p => category == null || p.Category == category)
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                },
+
+                CurrentCategory = category
+
+
+            };
+            return View(model); 
         }
     }
 }
